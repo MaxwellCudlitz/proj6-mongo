88908631ec62c752ebabf5da9fb6746756a44fb3
Maxwell Cudlitz <maxwell.cudlitz@gmail.com>
2017-11-15 01:22:16 -0800

    basic functionality implemented- beginning work on deletion

--- memos/flask_main.py
+++ memos/flask_main.py
@@ -76,32 +76,50 @@ except:
 @app.route("/index")
 def index():
 
-  #otherwise render page
   app.logger.debug("Main page entry")
-
-  # sort dates
-  g.memos = get_memos()
-  #g.memos.sort(key=lambda m: arrow.get(m['date']))
-
-  for memo in g.memos: 
-      app.logger.debug("Memo: " + str(memo))
-
+#   g.memos = get_memos()
+#   for memo in g.memos: 
+#       app.logger.debug("Memo: " + str(memo))
   return flask.render_template('index.html')
 
 
 @app.route("/_create_memo", methods=['POST'])
 def create_memo():
-    """creates a new memo, inserts it into mongo, and returns JSON"""
+    """
+    creates a new memo, inserts it into mongo, 
+    and returns the current memo list as 
+    formatted JSON
+    """
     form = request.form
     date = form['date']
     memo = form['memo']
     record = {
         "type": "dated_memo",
-        "date": str(arrow.get(date)),
+        "date": arrow.get(date).naive,
         "text": memo
     }
-    # collection.insert(record)
-    return flask.jsonify(get_memos())
+    collection.insert(record)
+    return retrieve_formatted_json_memos()
+
+@app.route("/_retrieve_memos", methods=['GET'])
+def retrieve_memos():
+    """
+    Returns formatted and humanized memos
+    for the client. Only called when the
+    page first loads.
+    """
+    return retrieve_formatted_json_memos()
+
+def retrieve_formatted_json_memos():
+    """
+    returns a JSOn object that contains memos that have 
+    been localized and humanized for the client
+    """
+    memos = get_memos()
+    for memo in memos:
+        memo['date'] = humanize_arrow_date(arrow.get(memo['date']))
+    return flask.jsonify(memos)
+
 
 
 @app.errorhandler(404)
@@ -117,7 +135,6 @@ def page_not_found(error):
 #
 #################
 
-
 @app.template_filter( 'humanize' )
 def humanize_arrow_date( date ):
     try:
@@ -131,6 +148,8 @@ def humanize_arrow_date( date ):
                 human = "Tomorrow"
     except: 
         human = date
+
+    print(human)
     return human
 
 
@@ -147,6 +166,10 @@ def get_memos():
     records = []
     for record in collection.find( { "type": "dated_memo" } ):
         record['date'] = arrow.get(record['date']).isoformat()
+
+        # stringifies ID so that it may be used for the 
+        # deltion callback in the rendered page- regular
+        # ID types do not play well with flask.jsonify().
         record['_id'] = str(record['_id'])
         records.append(record)
     records.sort(key=lambda m: arrow.get(m['date']))
